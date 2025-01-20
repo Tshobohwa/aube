861512d89e0fdf5684d0ecf5e1b07571312c4c03
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-01-20 07:40:54 +0200

    Add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className=" bg-yellow-700">
+      <Header />
       <p className="font-semibold underline text-3xl">Hello guys</p>
     </div>
   );
