2608a5f44b459923114884a4c400a87ca05afa3b
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-01-20 09:26:13 +0200

    Update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="w-full h-[6rem] bg-primary-950 flex items-center p-4 justify-between">
+    <header className="w-full h-[6rem] bg-primary-950 flex items-center p-4 justify-between">
       <h1 className="font-zen-dots text-white text-xl">aube</h1>
-    </div>
+      <nav className="font-poppins text-secondary-100">
+        <ul className="flex items-center gap-4">
+          <li>
+            <Link to={"/"}>
+              <p>home</p>
+            </Link>
+          </li>
+          <li>
+            <Link to={"/"}>
+              <p>about us</p>
+            </Link>
+          </li>
+          <li>
+            <Link to={"/"}>
+              <p>services</p>
+            </Link>
+          </li>
+          <li>
+            <Link to={"/"}>
+              <p>our works</p>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
