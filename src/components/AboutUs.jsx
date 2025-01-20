b5690591bc96f4cd653567a22a0dd464b2ec5a1d
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-01-20 18:28:19 +0200

    Create AboutUs component

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h2>Why working</h2>
+      <p>We focus on the deliverability of your product.</p>
+      <div></div>
+    </div>
+  );
+};
+
+export default AboutUs;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
+import AboutUs from "../components/AboutUs";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <p>Home</p>
+      <AboutUs />
     </div>
   );
 };
