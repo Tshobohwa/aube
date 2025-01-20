36d1082ac6ab92766280ff59aa0ba52308c25951
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-01-20 07:44:18 +0200

    Add custom secondary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        secondary: {
+          50: "#fffaeb",
+          100: "#fff1c6",
+          200: "#ffe088",
+          300: "#ffca4a",
+          400: "#ffb628",
+        },
+      },
+    },
   },
   plugins: [],
 };
