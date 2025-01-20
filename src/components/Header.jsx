2eb52f328b929466200d2af65f6ccec134a2c529
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-01-20 08:21:24 +0200

    Use flex

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="w-full h-[6rem] bg-primary-950 flex items-center p-4">
+    <div className="w-full h-[6rem] bg-primary-950 flex items-center p-4 justify-between">
       <h1 className="font-zen-dots text-white text-xl">aube</h1>
     </div>
   );
