88343a352e9301ae91d2a956d45dc29a64c33011
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-01-20 07:51:50 +0200

    Add custom primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,19 @@ export default {
   theme: {
     extend: {
       colors: {
+        primary: {
+          50: "#f1f9fa",
+          100: "#daeef3",
+          200: "#badde7",
+          300: "#8ac3d6",
+          400: "#52a1be",
+          500: "#3785a3",
+          600: "#306c8a",
+          700: "#2d5971",
+          800: "#2c4c5e",
+          900: "#284151",
+          950: "#182c3a",
+        },
         secondary: {
           50: "#fffaeb",
           100: "#fff1c6",
